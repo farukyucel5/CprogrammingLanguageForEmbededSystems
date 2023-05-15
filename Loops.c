853c3c2d8d75e3881a2e3bab0b1ed3fcb4452923
farukyucel5 <farukyucel5@gmail.com>
2023-05-15 19:27:48 +0300

    loops

---
+++ Loops.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+int input;
+
+/*while(input<10)
+{
+    scanf("%d",&input);
+
+}
+
+do{
+    scanf("%d",&input);
+}while(input<10);*/
+
+scanf("%d",&input);
+for(int i=0;i<input;i++){
+    printf("%d\n",i);
+}
+
+
+
+}
